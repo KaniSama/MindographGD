16135cd1a27451ebe40f815b138048992da6e917
KaniSama <naisyboy@gmail.com>
2023-12-07 09:07:41 +0500

    Converted [ AutosaveTimerLabel.gd , RMBMenu.gd ] to C#

---
+++ scripts/HUDElements/AutosaveTimerLabel.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class AutosaveTimerLabel : Label
+{
+	string initText;
+
+	public override void _Ready()
+	{
+		initText = Text;
+	}
+
+	public override void _Process(double delta)
+	{
+		int _time = FindParent("Workspace").GetTimeTillAutosave();
+
+		Text = initText + _time;
+	}
+
+	public override void _Input(InputEvent @event) {
+		if (@event.IsActionPressed("debug"))
+			Visible = !Visible;
+	}
+}
---
+++ scripts/HUDElements/RMBMenu.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class RMBMenu : ItemList
+{
+	public enum RmbIDs {
+		PIN = 0,
+		LINK = 1,
+		UNLINK = 3,
+		DELETE = 4,
+		NEW = 5,
+		COLOR = 7
+	};
+}
