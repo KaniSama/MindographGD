1d663bf50c83549425e6c03f15fa78f568441a6c
KaniSama <naisyboy@gmail.com>
2024-05-04 18:10:48 +0500

    Save/Load system overhaul Started

---
+++ scripts/Workspace/Modules/SaveLoadSystem.gd
@@ -0,0 +1,27 @@
+class_name SaveLoadSystem
+extends Node
+
+
+########################################### OVERRIDES
+func _____OVERRIDES(): pass
+
+
+
+########################################### HELPER FUNCTIONS
+func _____FUNCTIONALITY(): pass
+
+
+func save_project(project_name : String, project_data : Dictionary) -> void:
+	return
+
+func load_project(project_name : String) -> Dictionary:
+	return {}
+
+
+
+########################################### SIGNALS
+func _____SIGNALS(): pass
+
+
+func _on_child_key_pressed():
+	pass
